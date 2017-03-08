bf7f05e4e45ff38529124f9548ef488960cd90a7
Enrique Olvera <enrique.olveragarcia4@t-mobile.com>
2017-03-08 11:36:24 -0800

    change file directory

--- app/card/card.component.ts
+++ app/card/card.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     moduleId: module.id,
     selector: 'app-card',
-   // template: '<h1>Skeleton Project</h1>'
+   // template: '<h1>Skeleton Project</h1>' --inline
    templateUrl: 'card.component.html',
    styleUrls: [ 'card.component.css' ]
 })
