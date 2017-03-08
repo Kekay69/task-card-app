5ba541c611669a4e96de8fbd48e6fff7506ec482
Enrique Olvera <enrique.olveragarcia4@t-mobile.com>
2017-03-08 13:39:46 -0800

    modified selector

--- app/card/card.component.ts
+++ app/card/card.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     moduleId: module.id,
-    selector: 'my-app',
+    selector: 'app-card',
    // template: '<h1>Skeleton Project</h1>' --inline
    templateUrl: 'card.component.html',
    styleUrls: [ 'card.component.css' ]
